8823ad0a6f74a09aa5d061e7694058e03b458fbb
Johannes Reichle <jreichle@hm.edu>
2024-06-13 10:56:45 +0200

    Remove prints

--- benches/monte_carlo.rs
+++ benches/monte_carlo.rs
@@ -11,7 +11,6 @@ const PI_5_DECIMALS: u32 = 314159;
 fn estimate_pi<R: Rng + RngCore + SeedableRng>() -> u32 {
     fn is_precision_reached(count: u32, iterations: u32) -> bool {
         let estimate: f64 = (count as f64) * 4.0 / (iterations as f64);
-        println!("{}", estimate);
         (estimate * (u32::pow(10, 5)) as f64).trunc() as u32 == PI_5_DECIMALS
     }
 
@@ -21,11 +20,9 @@ fn estimate_pi<R: Rng + RngCore + SeedableRng>() -> u32 {
 
     while !is_precision_reached(count, iterations) {
         // TODO: implement and use rand::distributions::Uniform? - more efficient when same range
-        println!("{}", rng.next_u32());
         let x: f32 = rng.gen_range(0.0..1.0);
         let y: f32 = rng.gen_range(0.0..1.0);
         let p: f32 = x * x + y * y;
-        println!("{}, {} = {}", x, y, p);
 
         iterations += 1;
         if p < 1.0 {
