5c459a745c4f7e096c465d58c852a7a2dc708721
Daniel Soukup <daniel.soukup@gmx.de>
2024-06-18 11:15:20 +0200

    feat: simple main to generate testvectors

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,43 @@
-use lehmer::core::FastU32;
-use lehmer::monte::estimate_pi;
+use clap::{Parser, Subcommand};
+
+#[allow(unused_imports)]
+use lehmer::core::{
+    BoroshNiederreiter, Crawford, CrayRanf, FastU32, Fishman18, LEcuyer, Lehmer32, NaiveParkMiller,
+    NaiveParkMillerOld, NaiveU32, ParkMillerEfficient, Randu, Waterman, INMOS,
+};
+
+#[allow(unused_imports)]
+use lehmer::monte::{estimate_pi, estimate_pi_simd};
+use lehmer::test_data::generate_all;
+
+// Can be overwritten with the CLI Flag
+const SEED: u64 = 333;
+
+#[derive(Subcommand)]
+enum Action {
+    Generate,
+    Bench,
+}
+
+/// Either generate some testdata or do some manual monte carlo
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    /// Generate Testvector or do some manual benchmarking
+    command: Action,
+    /// Amount of multiples of min. bitstreamchunk length 2**20
+    iterations: u64,
+    /// Seed for the RNG
+    seed: Option<u64>,
+}
 
 fn main() {
-    println!("{}", estimate_pi::<FastU32>());
+    let args = Cli::parse();
+    let seed = args.seed.unwrap_or(SEED);
+
+    match args.command {
+        Action::Generate => generate_all(args.iterations, seed),
+        Action::Bench => println!("{}", estimate_pi::<FastU32>()),
+        //println!("{}", estimate_pi_simd::<FastU32>()),
+    }
 }
