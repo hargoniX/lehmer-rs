e8a7537bcf8fb438119c90877dc23a886d875c6c
Daniel Soukup <daniel.soukup@gmx.de>
2024-07-10 23:39:46 +0200

    feat: rust ffi

--- lehmer/src/c_ffi.rs
+++ lehmer/src/c_ffi.rs
@@ -1,6 +1,8 @@
 use crate::core::FastU32;
 use rand_core::RngCore;
+use std::ffi::{c_char, CString};
 use std::sync::atomic::AtomicU32;
+use testu01_sys::{unif01_CreateExternGenBits, unif01_DeleteExternGenBits, unif01_Gen};
 
 // Two variants for the rust ffi to be able to generate a new number each time:
 // 1. static mut the generator
@@ -16,4 +18,16 @@ pub unsafe extern "C" fn lehmer_next() -> u32 {
     let mut rng = <FastU32>::new(*seed);
     *seed = rng.next_u32();
     *seed
+}
+
+pub fn crush_it(
+    gen_func: unsafe extern "C" fn() -> u32,
+    crush: unsafe extern "C" fn(*mut unif01_Gen),
+) {
+    let gen_name = CString::new("FastU32").unwrap();
+    unsafe {
+        let gen = unif01_CreateExternGenBits(gen_name.as_ptr() as *mut c_char, Some(gen_func));
+        crush(gen);
+        unif01_DeleteExternGenBits(gen);
+    }
 }
